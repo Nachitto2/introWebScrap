9560ef1f932415e052fde96f511fb943dda26061
Ignacio <ignavenditto@gmail.com>
2026-01-14 15:52:41 -0300

    Corregimos un error en el join, un error en el uso del bs4, y agrego el link que me faltaba adentro del for

--- searchpypi.py
+++ searchpypi.py
@@ -1,17 +1,15 @@
 import requests, sys,webbrowser,bs4
 
-if len(sys.argv)>1:
-    res = requests.get('https://pypi.org/search/?q=' +' '.join(sys.argv[1:]))
-    res.raise_for_status()
+print("Searching...")
+res = requests.get('https://pypi.org/search/?q=' +' '.join(sys.argv[1:]))
+res.raise_for_status()
 
-    soup = bs4.BeautifulSoup(res.text,"html parser")
-    link_elem = soup.select(".package-snippet")
+soup = bs4.BeautifulSoup(res.text,"html.parser")
+link_elem = soup.select(".package-snippet")
 
-    num_open = min(5,len(link_elem))
+num_open = min(5,len(link_elem))
 
-    for i in range(num_open):
-        url_to_open= ""+ link_elem[i].get("href")
-        print("Opening...", url_to_open)
-        webbrowser(url_to_open)
-else:
-    print("Nada")
+for i in range(num_open):
+    url_to_open= "https://pypi.org/search/?q="+ link_elem[i].get("href")
+    print("Opening...", url_to_open)
+    webbrowser.open(url_to_open)
