f5f1f0e6c8b05e682548a0dc81b30273aee9d2a5
Ignacio <ignavenditto@gmail.com>
2026-01-14 15:41:46 -0300

    Corrijo el if

--- searchpypi.py
+++ searchpypi.py
@@ -1,6 +1,6 @@
 import requests, sys,webbrowser,bs4
 
-if sys.argv>1:
+if len(sys.argv)>1:
     res = requests.get('https://pypi.org/search/?q=' +' '.join(sys.argv[1:]))
     res.raise_for_status()
 
