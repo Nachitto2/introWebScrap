6433d2215340d54abb854227d631f95692e89a42
Ignacio <ignavenditto@gmail.com>
2026-01-14 08:55:12 -0300

    Explico funciones de webscrap

--- findLatLong.py
+++ findLatLong.py
@@ -4,7 +4,7 @@ import json
 city_name = 'Buenos Aires'
 state_code = 'BA'
 country_code= 'AR'
-API_KEY = '30ee784a80d81480dab1749d33980112'  # Not a real API key
+API_KEY = '30ee784a80d81480dab1749d33980112'  # Esta API key no es real
 url =f'https://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}'
 response = requests.get(url)
 print(response.text) 
@@ -14,4 +14,3 @@ if response.status_code == 200:
     response_data = json.loads(response.text)
     print(response_data)
 
-    
